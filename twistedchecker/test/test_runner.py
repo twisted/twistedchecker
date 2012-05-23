03244e8f171e9604836a487cf16f22de49d42323
zomux <raphael@uaca.com>
2012-05-24 00:26:12 +0900

    added runner

--- twistedchecker/test/test_runner.py
+++ twistedchecker/test/test_runner.py
@@ -1,12 +1,22 @@
+import sys
 import StringIO
 from twisted.trial import unittest
 
 from twistedchecker.core.runner import Runner
 
 class RunnerTestCase(unittest.TestCase):
-  def testRun():
-    resultOutput = StringIO.StringIO()
-    runner = Runner()
-    runner.setOutput(resultOutput)
-    runner.run([])
-    self.failUnless(len(resultOutput),"Fail to get result from runner")
+
+    def testRun(self):
+        resultOutput = StringIO.StringIO()
+        stdout = sys.stdout
+        sys.stdout = resultOutput
+        runner = Runner()
+        runner.setOutput(resultOutput)
+        try:
+            self.assertRaises(SystemExit,runner.run,["--version"])
+            sys.stdout = stdout
+        except:
+            sys.stdout = stdout
+            self.fail("Could not load runner")
+
+
