2a170abbf41f9a6cb7b910a2b641d65754f74d4e
zomux <raphael@uaca.com>
2012-05-25 23:23:48 +0900

    fixed some path issues

---
+++ twistedchecker/functionaltests/__init__.py
@@ -0,0 +1,3 @@
+"""
+Functional tests of TwistedChecker.
+"""
---
+++ twistedchecker/functionaltests/indentation.py
@@ -0,0 +1,12 @@
+# Indentation must be 4 spaces.
+# okay
+for i in range(10):
+    pass
+
+# indent of 2 spaces
+for i in range(10):
+  pass
+
+# indent of a tab
+for i in range(10):
+	pass
