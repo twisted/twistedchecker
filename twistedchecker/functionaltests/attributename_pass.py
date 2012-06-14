f15905e8cfccdb30169e7045e25c76befff77dd0
zomux <raphael@uaca.com>
2012-06-14 21:01:40 +0900

    removed trailing spaces

--- twistedchecker/functionaltests/attributename_pass.py
+++ twistedchecker/functionaltests/attributename_pass.py
@@ -2,9 +2,9 @@
 
 class FooClass:
     """
-    Attribute names should be in mixed case, 
+    Attribute names should be in mixed case,
     with the first letter lower case,
-    each word separated by having its first letter capitalized 
+    each word separated by having its first letter capitalized
     just like method names.
     And all private names should begin with an underscore.
     """
