1e799323f8877fbeee368d19a3a9c314e5e05af3
zomux <raphael@uaca.com>
2012-06-19 08:17:18 +0900

    Fixed https://code.launchpad.net/~zomux/twistedchecker/checktrailingspace/+merge/107976/comments/237788

--- twistedchecker/checkers/pep8format.py
+++ twistedchecker/checkers/pep8format.py
@@ -32,6 +32,8 @@ class PEP8Checker(BaseChecker):
         'W291': ('W9010', ''),
         'W293': ('W9011', ''),
     }
+    warnings = None
+    pep8Checker = None
 
 
     def visit_module(self, node):
@@ -41,37 +43,56 @@ class PEP8Checker(BaseChecker):
         self._runPEP8Checker(node.file)
 
 
+    def _recordErrors(self, lineNumber, offset, text, check):
+        """
+        A hack function to replace report_error in pep8.
+        And record output warings.
+
+        @param lineNumber: line number
+        @param offset: column offset
+        @param text: warning message
+        @param check: check object in pep8
+        """
+        code = text[:4]
+        self.warnings.append((self.pep8Checker.line_offset + lineNumber,
+                              offset + 1, code, text))
+
+
     def _runPEP8Checker(self, file):
         """
         Call the checker of pep8
+
+        @param file: path of module to check
         """
         pep8.options = pep8.process_options([file])[0]
         checker = pep8.Checker(file)
+        checker.report_error = self._recordErrors
         # backup stdout
-        bakStdout = sys.stdout
+        stdoutBak = sys.stdout
+        self.warnings = []
+        self.pep8Checker = checker
         # set a stream to replace stdout, and get results in it
         streamResult = StringIO.StringIO()
         sys.stdout = streamResult
         checker.check_all()
-        sys.stdout = bakStdout
-        self._outputMessages(streamResult.getvalue())
+        sys.stdout = stdoutBak
+        self._outputMessages()
 
 
-    def _outputMessages(self, pep8result):
+    def _outputMessages(self):
         """
         Map pep8 results to messages in pylint, then output them.
-
-        @param pep8result: results of pep8
         """
-        linesResult = [l for l in pep8result.split("\n") if l]
-        for line in linesResult:
-            msgidInPEP8 = line.split(" ")[1]
+        if not self.warnings:
+            # no warnings were found
+            return
+        for warning in self.warnings:
+            linenum, offset, msgidInPEP8, text = warning
             if msgidInPEP8 in self.mapPEP8Messages:
                 msgid, patternArguments = self.mapPEP8Messages[msgidInPEP8]
-                linenum = line.split(":")[1]
                 arguments = []
                 if patternArguments:
-                    matchResult = re.search(patternArguments, line)
+                    matchResult = re.search(patternArguments, text)
                     if matchResult:
                         arguments = matchResult.groups()
                 self.add_message(msgid, line=linenum, args=arguments)
