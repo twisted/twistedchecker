6df18b635b0e0b0cea703f40c6c9465e6d38c8ac
zomux <raphael@uaca.com>
2012-07-02 19:25:13 +0900

    fixed https://code.launchpad.net/~zomux/twistedchecker/checkvariablename/+merge/111817/comments/242426

---
+++ twistedchecker/functionaltests/variablename_fail.py
@@ -0,0 +1,14 @@
+# enable: C0103
+
+class FooClass:
+    """
+    Just a class.
+    """
+    def __init__(self):
+        """
+        Some bad variable names.
+        """
+        FooBar = None
+        Foo = None
+        FOO = None
+        foo_bar = None
