c96259ca8986062d00850f28c86ab23c7e45c0e8
zomux <raphael@uaca.com>
2012-07-02 19:07:57 +0900

    fixed https://code.launchpad.net/~zomux/twistedchecker/checktrailingspace/+merge/107976/comments/242555

--- twistedchecker/checkers/pep8format.py
+++ twistedchecker/checkers/pep8format.py
@@ -11,6 +11,55 @@ from pylint.reporters import diff_string
 from pylint.checkers import BaseChecker, EmptyReport
 
 import pep8
+from pep8 import Checker as PEP8OriginalChecker
+
+class PEP8WarningRecorder(PEP8OriginalChecker):
+    """
+    A subclass of pep8 checker and will record warnings.
+    """
+    warnings = None
+
+    def __init__(self, file):
+        """
+        Initate warnings.
+
+        @param file: file to be checked
+        """
+        pep8.options = pep8.process_options([file])[0]
+        PEP8OriginalChecker.__init__(self, file)
+        self.warnings = []
+        self.run()
+
+
+    def report_error(self, lineNumber, offset, text, check):
+        """
+        A function to override report_error in pep8.
+        And record output warings.
+
+        @param lineNumber: line number
+        @param offset: column offset
+        @param text: warning message
+        @param check: check object in pep8
+        """
+        code = text[:4]
+        self.warnings.append((self.line_offset + lineNumber,
+                              offset + 1, code, text))
+
+
+    def run(self):
+        """
+        Run pep8 checker and record warnings.
+        """
+        # set a stream to replace stdout, and get results in it
+        stdoutBak = sys.stdout
+        streamResult = StringIO.StringIO()
+        sys.stdout = streamResult
+        try:
+            PEP8OriginalChecker.check_all(self)
+        finally:
+            sys.stdout = stdoutBak
+
+
 
 class PEP8Checker(BaseChecker):
     """
@@ -43,50 +92,27 @@ class PEP8Checker(BaseChecker):
         self._runPEP8Checker(node.file)
 
 
-    def _recordErrors(self, lineNumber, offset, text, check):
-        """
-        A hack function to replace report_error in pep8.
-        And record output warings.
-
-        @param lineNumber: line number
-        @param offset: column offset
-        @param text: warning message
-        @param check: check object in pep8
-        """
-        code = text[:4]
-        self.warnings.append((self.pep8Checker.line_offset + lineNumber,
-                              offset + 1, code, text))
-
-
     def _runPEP8Checker(self, file):
         """
         Call the checker of pep8
 
         @param file: path of module to check
         """
-        pep8.options = pep8.process_options([file])[0]
-        checker = pep8.Checker(file)
-        checker.report_error = self._recordErrors
-        # backup stdout
-        stdoutBak = sys.stdout
-        self.warnings = []
-        self.pep8Checker = checker
-        # set a stream to replace stdout, and get results in it
-        streamResult = StringIO.StringIO()
-        sys.stdout = streamResult
-        checker.check_all()
-        sys.stdout = stdoutBak
-        self._outputMessages()
+        recorder = PEP8WarningRecorder(file)
+        self._outputMessages(recorder.warnings)
 
 
-    def _outputMessages(self):
+    def _outputMessages(self, warnings):
         """
         Map pep8 results to messages in pylint, then output them.
+
+        @param warnings: it should be a list of tuple including
+        line number and message id
         """
-        if not self.warnings:
+        if not warnings:
             # no warnings were found
             return
-        for warning in self.warnings:
+        for warning in warnings:
             linenum, offset, msgidInPEP8, text = warning
             if msgidInPEP8 in self.mapPEP8Messages:
                 msgid, patternArguments = self.mapPEP8Messages[msgidInPEP8]
