6d81206847ece377dbd48f85e4272fabd3a06b31
zomux <raphael@uaca.com>
2012-07-17 19:10:08 +0900

    added test for docstring

---
+++ twistedchecker/test/test_docstring.py
@@ -0,0 +1,25 @@
+import sys
+import os
+import StringIO
+
+from twisted.trial import unittest
+
+from twistedchecker.checkers.docstring import DocstringChecker
+
+
+class DocstringTestCase(unittest.TestCase):
+    """
+    Test for twistedchecker.checkers.docstring
+    """
+
+    def test_getLineIndent(self):
+        """
+        Test of twistedchecker.checkers.docstring._getLineIndent.
+        """
+        checker = DocstringChecker()
+        indentNoSpace = checker._getLineIndent("foo")
+        indentTwoSpaces = checker._getLineIndent("  foo")
+        indentFourSpaces = checker._getLineIndent("    foo")
+        self.assertEqual(indentNoSpace, 0)
+        self.assertEqual(indentTwoSpaces, 2)
+        self.assertEqual(indentFourSpaces, 4)
