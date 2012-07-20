c955e809428ad7e4708efa5107dc15134f671266
zomux <raphael@uaca.com>
2012-07-20 09:09:31 +0900

    check function names

---
+++ twistedchecker/functionaltests/functionname_fail.py
@@ -0,0 +1,18 @@
+# enable: C0103
+
+# Function names should be same as method names
+
+def FooBar(self):
+    pass
+
+
+def Foo(self):
+    pass
+
+
+def FOOBAR(self):
+    pass
+
+
+def fooBar_of_baz(self):
+    pass
---
+++ twistedchecker/functionaltests/functionname_pass.py
@@ -0,0 +1,22 @@
+# enable: C0103
+
+# Function names should be same as method names
+
+def fooBar(self):
+    pass
+
+
+def foo(self):
+    pass
+
+
+def _foo(self):
+    pass
+
+
+def _fooBar(self):
+    pass
+
+
+def get_fooBar(self):
+    pass
