be1247af45dba81497e18516968e17b5be1f76dc
zomux <raphael@uaca.com>
2012-07-26 08:29:47 +0900

    address problems in https://code.launchpad.net/~zomux/twistedchecker/checkpy3-haskey/+merge/112920/comments/250798

--- twistedchecker/checkers/python3.py
+++ twistedchecker/checkers/python3.py
@@ -65,36 +65,46 @@ class Python3Checker(BaseChecker):
         @parm node: current node of checking
         """
         issueFound = False
-        try:
-            # get the function
-            func = node.func
-            # get attribute name, if an error is generated here,
-            # means that the node is a function call,
-            # we should filter these usages and capture dict.has_key
-            attrname = func.attrname
-            # check whether this method is has_key
-            if attrname != "has_key":
-                return
-            # now get the object which is called
-            # it should be the first child of the method node
-            objCalled = func.get_children().next()
-            if isinstance(objCalled, logilab.astng.node_classes.Dict):
-                # in this case, the statement should like
-                # {}.has_key()
-                issueFound = True
-            else:
-                # check for foo.has_key() and foo is defined as a dict
-                # elsewhere
-                # if an error is generated here, it means ast failed to
-                # find the definition
-                objInfered = node.func.last_child().infered()[0]
-                if isinstance(objInfered, logilab.astng.node_classes.Dict):
-                    issueFound = True
-        except AttributeError:
+        # get the function
+        func = node.func
+        # get attribute name, if the node does not have 'attrname',
+        # means that the node is a function call,
+        # we should filter these usages and capture dict.has_key
+        if not hasattr(func,"attrname"):
             return
-        except ASTNGError:
-            # ignore errors from astng
-            pass
+        attrname = func.attrname
+        # check whether this method is has_key
+        if attrname != "has_key":
+            return
+        # now get the object which is called
+        # it should be the first child of the method node
+        objCalled = func.get_children().next()
+        if isinstance(objCalled, logilab.astng.node_classes.Dict):
+            # in this case, the statement should like
+            # {}.has_key()
+            issueFound = True
+        else:
+            # check for foo.has_key() and foo is defined as a dict
+            # elsewhere
+            # if an error is generated here, it means ast failed to
+            # find the definition
+            nodeArgument = node.func.last_child()
+            if not nodeArgument:
+                # no argument is used
+                return
+            try:
+                objInferedList = nodeArgument.infered()
+            except ASTNGError:
+                # may be the name is unresolvable,
+                # or defination is unreachable
+                return
+            if not objInferedList:
+                # no infered node is found
+                return
+            objInfered = objInferedList[0]
+            if isinstance(objInfered, logilab.astng.node_classes.Dict):
+                issueFound = True
+
         if issueFound:
             self.add_message('W9602', node=node)
 
