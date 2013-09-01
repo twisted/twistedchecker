63280c4f3d3be379d0f4d686a55cf62f7b727d18
HawkOwl <hawkowl@outlook.com>
2013-09-02 05:46:43 +0800

    Updated setup.py according to review, changed it to use README.rst instead

--- setup.py
+++ setup.py
@@ -3,66 +3,41 @@
 # Copyright (c) Twisted Matrix Laboratories.
 # See LICENSE for details.
 
-"""
-Distutils installer for TwistedChecker.
-"""
-
-import sys
-
-try:
-    from setuptools import setup
-except ImportError:
-    pass
+from setuptools import find_packages, setup
 
 
-
-def main(args):
-    """
-    Install TwistedChecker.
-    """
-    setup(
-        name='TwistedChecker',
-        version='0.1.0',
-        author='Twisted Matrix Laboratories',
-        author_email='twisted-python@twistedmatrix.com',
-        packages=[
-            'twistedchecker',
-            'twistedchecker.configuration',
-            'twistedchecker.functionaltests',
-            'twistedchecker.test',
-            'twistedchecker.checkers',
-            'twistedchecker.core',
-            'twistedchecker.reporters'
-            ],
-        data_files=[
-            ('twistedchecker/configuration', ['twistedchecker/configuration/pylintrc'])
-            ],
-        scripts=['bin/twistedchecker'],
-        url='http://pypi.python.org/pypi/TwistedChecker/',
-        license='MIT',
-        classifiers=[
-            "Development Status :: 3 - Alpha",
-            "Environment :: Console",
-            "Intended Audience :: Developers",
-            "License :: OSI Approved :: MIT License",
-            "Programming Language :: Python",
-            "Topic :: Software Development :: Quality Assurance"
-            ],
-        install_requires=[
-            "pylint==0.26.0",
-            "pep8==1.3.3"
-            ],
-        description='A code standards checker for Twisted.',
-        long_description="""\
-TwistedChecker is a code standards checker, originally designed for the use of
-the Twisted project. It tests Python code for problems such as PEP8 compliance,
-docstrings, trailing spaces and strange variable naming.
-""",
-        )
-
-
-if __name__ == "__main__":
-    try:
-        main(sys.argv[1:])
-    except KeyboardInterrupt:
-        sys.exit(1)
+setup(
+    name='TwistedChecker',
+    description='A Twisted coding standard compliance checker.',
+    version='0.1.0',
+    author='Twisted Matrix Laboratories',
+    author_email='twisted-python@twistedmatrix.com',
+    url='https://launchpad.net/twistedchecker',
+    packages=find_packages(),
+    package_data={
+        "twistedchecker": ["configuration/pylintrc"]
+        },
+    scripts=[
+        'bin/twistedchecker'
+        ],
+    license='MIT',
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Software Development :: Quality Assurance"
+        ],
+    keywords=[
+        "twisted", "checker", "compliance", "pep8"
+        ],
+    install_requires=[
+        "pylint == 0.26.0",
+        "pep8"
+        ],
+    long_description=file('README.rst').read()
+    )
