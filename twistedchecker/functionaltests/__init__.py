45cd0d427d13bffa6e31752f4d964f2a8e547fe5
Richard Wall <richard@the-moon.net>
2013-09-09 21:43:48 +0100

    fix functionaltests test header

--- twistedchecker/functionaltests/__init__.py
+++ twistedchecker/functionaltests/__init__.py
@@ -1,4 +1,4 @@
-# -*- test-case-name: twistedchecker.test.test_functionalTestRunner -*-
+# -*- test-case-name: twistedchecker.test.test_functionaltests -*-
 # Copyright (c) Twisted Matrix Laboratories.
 # See LICENSE for details.
 
