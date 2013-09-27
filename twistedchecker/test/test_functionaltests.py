8727f09371fcd0f7de43a89cb7f397414bc66ce7
Richard Wall <richard@the-moon.net>
2013-09-27 17:04:00 +0100

    start from scratch using a custom partial function and type to dynamically build a test class

--- twistedchecker/test/test_functionaltests.py
+++ twistedchecker/test/test_functionaltests.py
@@ -5,206 +5,59 @@
 Test cases for running the functional tests in
 L{twistedchecker.functionaltests}.
 """
-
-import itertools
-import os
-import StringIO
+from functools import update_wrapper
 
 from twisted.trial import unittest
 
-import twistedchecker
-from twistedchecker.core.runner import Runner
-from twistedchecker.reporters.test import TestReporter
+
+
+def functionalTester(testCase, a, b):
+    testCase.assertEqual(a, b)
 
 
 
-def listAllTestModules():
+def _partial2(wrapped, *partialArgs, **partialKwargs):
     """
-    Discover all the functional test modules.
+    A custom implementation of L{functools.partial} which returns a function
+    instead of a partial object.  This allows it to be assigned to a class
+    attribute and used as an instance method.
 
-    Modules whose name begin with an underscore are ignored.
+    @param wrapped: The function whose arguments will be partially applied
+    @type wrapped: L{callable}
 
-    @return: A list of test file paths and module names
-    @rtype: L{list} of 2-L{tuple}(L{str}, L{str})
+    @param partialArgs: The positional arguments which will be applied to
+        C{wrapped}.
+    @type partialArgs: L{tuple}
+
+    @param partialKwargs: The keyword arguments which will be applied to
+        C{wrapped}.
+    @type partialKwargs: L{dict}
+
+    @return: A wrapper function which will apply the supplied arguments to
+        C{wrapped} when it is called.
+    @rtype: L{callable}
     """
-    testmodules = []
-    pathTestModules = os.path.join(twistedchecker.abspath,
-                                   "functionaltests")
-    for root, dirs, files in os.walk(pathTestModules):
-        for testfile in files:
-            if testfile.startswith("_"):
-                continue
-            if testfile.endswith(".py"):
-                pathFile = os.path.join(twistedchecker.abspath,
-                                        root, testfile)
-                pathRelative = os.path.relpath(pathFile,
-                                               twistedchecker.abspath)
-                modulename = "twistedchecker." + \
-                    pathRelative.replace(".py", "").replace(os.sep, ".")
-                testmodules.append((pathFile, modulename))
-    return testmodules
+    def wrapper(*args, **kwargs):
+        args = args + partialArgs[len(args):]
+        kwargs.update(partialKwargs)
+        return wrapped(*args, **kwargs)
+    return update_wrapper(wrapper, wrapped)
 
 
 
-def _limitMessages(testfile, runner):
-    """
-    Enable or disable messages according to the testfile.
-    The first line of testfile should in format of:
-    # enable/disable: [Message ID], ...
-
-    @param testfile: testfile to read, enable and disable infomation should
-        in the first line of it.
-    @param runner: current runner for checking testfile.
-    """
-    firstline = open(testfile).readline()
-    if "enable" not in firstline and "disable" not in firstline:
-        # Could not find enable or disable messages
-        return
-    action, messages = firstline.lstrip("#").strip().split(":", 1)
-    messages = [msgid for msgid in messages.strip().split(",") if msgid]
-    action = action.strip()
-
-    if action == "enable":
-        # Disable all other messages
-        runner.linter.disable_noerror_messages()
-        for msgid in messages:
-            runner.linter.enable(msgid)
-    else:
-        for msgid in messages:
-            runner.linter.disable(msgid)
+def tests():
+    t = []
+    x = iter([1,2,3,4,5,6])
+    for i in x:
+        i2 = next(x)
+        t.append(
+            ('test_foo%s' % (i,), _partial2(functionalTester, a=i, b=i2)))
+    return dict(t)
 
 
 
-def _getChecker(runner, checkerName):
-    """
-    Get a specified checker from a Runner object.
-
-    @param runner: an instance of Runner
-    @param checkerName: the name of a checker
-    @return: a checker object
-    """
-    for checker in runner.linter.get_checkers():
-        if hasattr(checker, "name") and checker.name == checkerName:
-            return checker
-    return None
-
-
-
-def _testNameFromModuleName(moduleName):
-    """
-    Mangle a module name so it can be used as a test function name.
-
-    @param moduleName: The qualified module name.
-    @type moduleName: L{str}
-
-    @return: The test name derived from the supplied C{moduleName}.
-    @rtype: L{str}
-    """
-    return 'test_' + moduleName.replace('.', '_')
-
-
-
-def _buildTestMethod(testFilePath, moduleName):
-    """
-    Create a closure function which will call C{_runTest} on its
-    parent instance, supplying the arguments enclosed in this function
-    call.
-
-    @param testFilePath: The path to the sample module to test.
-    @type testFilePath: L{str}
-
-    @param moduleName: The qualified module name.
-    @type moduleName: L{str}
-
-    @return: The closure function
-    @rtype: L{function}
-    """
-    return lambda self: self._runTest(testFilePath, moduleName)
-
-
-
-def _addFunctionalTests(testCaseClass):
-    """
-    Discover functional test modules and add test methods to a
-    TestCase class for each discovered functional test.
-
-    These test methods can be discovered and run individually by
-    trial.
-
-    @param testCaseClass: The class to which test methods will be
-        added.
-    @type testCaseClass: L{type}
-
-    @return: The modified C{testCaseClass}
-    @rtype: L{type}
-    """
-    for testFilePath, moduleName in listAllTestModules():
-        setattr(
-            testCaseClass,
-            _testNameFromModuleName(moduleName),
-            _buildTestMethod(testFilePath, moduleName)
-            )
-    return testCaseClass
-
-
-@_addFunctionalTests
-class FunctionalTests(unittest.TestCase):
-    """
-    A TestCase to which functional test methods will be dynamically
-    added at module load time.
-    """
-    def _runTest(self, testFilePath, moduleName):
-        """
-        Run a functional test.
-
-        @param testFilePath: The path to the module to test.
-        @type testFilePath: L{str}
-
-        @param moduleName: The qualified name of the module to test.
-        @type moduleName: L{str}
-        """
-        pathResultFile = testFilePath.replace(".py", ".result")
-
-        self.assertTrue(
-            os.path.exists(testFilePath),
-            msg="could not find testfile: %r" % (testFilePath,))
-        self.assertTrue(
-            os.path.exists(pathResultFile),
-            msg="could not find resultfile: %r" % (pathResultFile,))
-
-        outputStream = StringIO.StringIO()
-        runner = Runner()
-        runner.allowOptions = False
-        runner.setOutput(outputStream)
-
-        # Set the reporter to
-        # C{twistedchecker.reporters.test.TestReporter}
-        runner.setReporter(TestReporter())
-        _limitMessages(testFilePath, runner)
-
-        # Enable pep8 checking
-        pep8Checker = _getChecker(runner, "pep8")
-        if pep8Checker:
-            pep8Checker.pep8Enabled = True
-
-        # Run the test
-        runner.run([moduleName])
-
-        # Check the results
-        expectedResult = open(pathResultFile).read().splitlines()
-        outputResult = outputStream.getvalue().splitlines()
-
-        try:
-            self.assertEqual(expectedResult, outputResult)
-        except unittest.FailTest:
-            # Format the results side by side for easy comparison
-            i = itertools.izip_longest(
-                ['= Expected ='] + expectedResult,
-                ['= Actual ='] + outputResult, fillvalue='')
-
-            output = ['', moduleName]
-            for col1, col2 in i:
-                output.append(col1.ljust(20) + col2)
-            output.append('')
-
-            self.fail('\n'.join(output))
+FunctionalTests = type(
+    "FunctionalTests",
+    (unittest.TestCase,),
+    tests()
+)
