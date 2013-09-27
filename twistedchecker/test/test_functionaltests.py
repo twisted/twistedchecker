96f4e4c256fdbfd66241581a38b660718be4c04e
Richard Wall <richard@the-moon.net>
2013-09-27 19:12:31 +0100

    run a single test

--- twistedchecker/test/test_functionaltests.py
+++ twistedchecker/test/test_functionaltests.py
@@ -6,13 +6,15 @@ Test cases for running the functional tests in
 L{twistedchecker.functionaltests}.
 """
 from functools import update_wrapper
+import io
+import itertools
+import os
 
+from twisted.python.reflect import filenameToModuleName
 from twisted.trial import unittest
 
-
-
-def functionalTester(testCase, a, b):
-    testCase.assertEqual(a, b)
+from twistedchecker.core.runner import Runner
+from twistedchecker.reporters.test import TestReporter
 
 
 
@@ -45,13 +47,65 @@ def _partial2(wrapped, *partialArgs, **partialKwargs):
 
 
 
+def _formatResults(moduleName, expectedResult, actualResult):
+    """
+    Format the results side by side for easy comparison
+    """
+    i = itertools.izip_longest(
+        ['= Expected ='] + expectedResult.splitlines(),
+        ['= Actual ='] + actualResult.splitlines(), fillvalue='')
+
+    output = ['', moduleName]
+    for col1, col2 in i:
+        output.append(col1.ljust(20) + col2)
+    return os.linesep.join(output)
+
+
+
+def _runTest(testCase, testFilePath):
+    """
+    Run a functional test.
+
+    @param testFilePath: The path to the module to test.
+    @type testFilePath: L{str}
+    """
+    pathResultFile = testFilePath.replace(".py", ".result")
+    moduleName = filenameToModuleName(testFilePath)
+    outputStream = io.BytesIO()
+
+    runner = Runner()
+    runner.allowOptions = False
+    runner.setOutput(outputStream)
+    runner.setReporter(TestReporter())
+    runner.run([moduleName])
+
+    # Check the results
+    expectedResult = open(pathResultFile).read()
+    outputResult = outputStream.getvalue()
+
+    try:
+        testCase.assertEqual(expectedResult, outputResult)
+    except unittest.FailTest:
+        testCase.fail(_formatResults(moduleName, expectedResult, outputResult))
+
+#    _limitMessages(testFilePath, runner)
+# Enable pep8 checking
+# pep8Checker = _getChecker(runner, "pep8")
+# if pep8Checker:
+#     pep8Checker.pep8Enabled = True
+# Run the test
+
+
 def tests():
     t = []
-    x = iter([1,2,3,4,5,6])
-    for i in x:
-        i2 = next(x)
-        t.append(
-            ('test_foo%s' % (i,), _partial2(functionalTester, a=i, b=i2)))
+    t.append(
+        ('test_foo',
+         _partial2(
+             _runTest,
+             testFilePath=('/home/richard/projects/TwistedChecker/'
+                           'branches/isolated-functional-tests-1010392/'
+                           'twistedchecker/functionaltests/argumentname.py')))
+    )
     return dict(t)
 
 
