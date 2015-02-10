0b5aa0de48ebf81d9eb2909f1e4df77d8a050ebe
Adi Roiban <adi.roiban@chevah.com>
2015-02-10 13:32:56 +0000

    Initia patch for long lines.

--- twistedchecker/functionaltests/maxlinelength.py
+++ twistedchecker/functionaltests/maxlinelength.py
@@ -1,3 +1,11 @@
 # enable: C0301
 # max line length of Twsited projects should be 79
 print "this line is long long  long  long long long long long ends at column 80"
+
+# But comments with url should be excepted.
+# http://thisurlisverylongandwillneverfitinto80columnsnomatterwhat.example.com/somegreatcontent.tml
+
+""""
+Long url form docstrings are ignored.
+See U{http://thisurlisverylongandwillneverfitinto80columnsnomatterwhat.example.com/somegreatcontent.html}
+"""
