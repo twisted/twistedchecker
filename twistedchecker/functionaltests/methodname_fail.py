68af4e723e4f977e9fd016eb80d73e8b661d19dd
Adi Roiban <adi.roiban@chevah.com>
2015-06-12 16:04:18 +0100

    Move all test in a single file.

--- twistedchecker/functionaltests/methodname_fail.py
+++ twistedchecker/functionaltests/methodname_fail.py
@@ -49,3 +49,47 @@ class foo:
 
     def __hiddenMethods_not_all_camel(self):
         pass
+
+
+
+class GoodMethodNames:
+
+    def test(self):
+        pass
+
+
+    def _test(self):
+        pass
+
+
+    def _testMethod(self):
+        pass
+
+
+    def get_testMethod(self):
+        pass
+
+
+    def _FOOBAR(self):
+        pass
+
+
+    def test_SOME_THING(self):
+        """
+        In Twisted we have methods like render_POST, xmlrpc_SOMETHING and due to this
+        test methods like test_render_POSTSomething are valid.
+        """
+        pass
+
+
+    def test_render_POSTSomething(self):
+        pass
+
+
+    def _normalPrivateMethod(self):
+        pass
+
+
+    def __hiddenMethod(self):
+        pass
+
