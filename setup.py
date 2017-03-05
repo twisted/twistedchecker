6ab6cfc3c1043f109fe0c890a034e983eddebea8
Craig Rodrigues <rodrigc@FreeBSD.org>
2017-03-05 00:23:04 -0800

    Previous version bump was too large, bump to 0.7.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.rst') as f:
 setup(
     name='twistedchecker',
     description='A Twisted coding standard compliance checker.',
-    version='16.0',
+    version='0.7.1',
     author='Twisted Matrix Laboratories',
     author_email='twisted-python@twistedmatrix.com',
     url='https://github.com/twisted/twistedchecker',
