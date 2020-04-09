bd3b09b3a0dc7cc450d4a6806802712d521b2d22
Craig Rodrigues <rodrigc@FreeBSD.org>
2020-04-09 12:56:16 -0700

    Remove Python 2

--- setup.py
+++ setup.py
@@ -29,8 +29,6 @@ setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.5",
         "Topic :: Software Development :: Quality Assurance"
