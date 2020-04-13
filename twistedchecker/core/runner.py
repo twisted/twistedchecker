2c1c31a8f91672e28d7c5ad209c627d20cefb83a
Craig Rodrigues <rodrigc@FreeBSD.org>
2020-04-13 02:52:26 -0700

    Get rid of setReporter() method and just use set_reporter()

--- twistedchecker/core/runner.py
+++ twistedchecker/core/runner.py
@@ -69,7 +69,7 @@ class Runner():
         # set default output stream to stdout
         self.setOutput(sys.stdout)
         # set default reporter to limited reporter
-        self.setReporter(LimitedReporter(allowedMessages))
+        self.linter.set_reporter(LimitedReporter(allowedMessages))
 
 
     def _makeOptions(self):
@@ -104,15 +104,6 @@ class Runner():
         """
         self.outputStream = stream
         sys.stdout = stream
-
-
-    def setReporter(self, reporter):
-        """
-        Set the reporter of pylint.
-
-        @param reporter: reporter used to show messages
-        """
-        self.linter.set_reporter(reporter)
 
 
     def displayHelp(self):
