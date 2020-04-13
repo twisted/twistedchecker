63c9d958185b70ac75009cc29ebe1f6877cc4e23
Craig Rodrigues <rodrigc@FreeBSD.org>
2020-04-13 02:10:59 -0700

    Update test results to use pylint.reporters.text.TextReporter

--- twistedchecker/test/test_runner.py
+++ twistedchecker/test/test_runner.py
@@ -440,7 +440,7 @@ W9001:  1,0: Missing copyright header
         exitResult = self.assertRaises(SystemExit, runner.run, [moduleName])
 
         os.chdir(workingDir)
-        predictResult = "7:C9302\n11:C0103\n14:C0103\n15:C9302\n"
+        predictResult = "************* Module temp.test\n7:C9302\n11:C0103\n14:C0103\n15:C9302\n"
         outputResult = self.outputStream.getvalue()
         self.assertEqual(outputResult, predictResult)
         self.assertEqual(16, exitResult.code)
